6288875fcc5ce7e516edf42874a4d260b23d9e1f
Alexey Khaiminov <a.khaiminov@corp.mail.ru>
2021-03-25 22:42:52 +0300

    update version

--- source/feature/measure/presentation/src/main/kotlin/com/github/xvar/presentation/ui/screen/ChooseTechScreen.kt
+++ source/feature/measure/presentation/src/main/kotlin/com/github/xvar/presentation/ui/screen/ChooseTechScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
